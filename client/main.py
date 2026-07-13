fbe3fbb7f7d69cb6547e90f2cc32934bf82cfe8a
trojas-gnister <troy.sparks@workshield.com>
2026-07-13 16:49:03 -0500

    feat: frame time logic

--- client/main.py
+++ client/main.py
@@ -1,18 +1,34 @@
 import pygame
 import sys
-from shared.config import SCREEN_H, SCREEN_W
+from shared.config import SCREEN_H, SCREEN_W, FPS_CAP, FIXED_DT
 
 
 def main() -> None:
+    clock = pygame.time.Clock()
+    accumulator = 0.0
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_W, SCREEN_H))
 
-    while True:
+    def update(dt):
+        pass
+
+    def render(alpha):
         screen.fill("black")
+        pygame.display.flip()
+
+    while True:
+        frame_time = clock.tick(FPS_CAP) / 1000.0
+        frame_time = min(frame_time, 0.25)
+        accumulator = +frame_time
+        while accumulator >= FIXED_DT:
+            update(FIXED_DT)
+            accumulator -= FIXED_DT
+        alpha = accumulator / FIXED_DT
+        render(alpha)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-        pygame.display.flip()
 
 
 if __name__ == "__main__":
